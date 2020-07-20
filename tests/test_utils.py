33a0d90ad9218da8d34d6daa71d473587b8443db
Aaron Markey <aaron.markey@leaflink.com>
2020-07-20 12:22:38 -0400

    * JsonErrorSourcePointer now expects a tuple of strs instead of a str. * flatten_dict() returns tuple, Any instead of str, Any.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,29 +5,25 @@ from pyll_json_errors import utils
 
 
 @pytest.mark.parametrize(
-    "inp, outp, prefix, sep",
+    "inp, outp",
     [
         (
             {"one": 1, "two": [2, 2], "three": {"three_one": 31, "three_two": {"0": 320, "1": 321, "2": {"four": 4}}}},
             {
-                "/one": 1,
-                "/two": [2, 2],
-                "/three.three_one": 31,
-                "/three.three_two.0": 320,
-                "/three.three_two.1": 321,
-                "/three.three_two.2.four": 4,
+                ("one",): 1,
+                ("two",): [2, 2],
+                ("three", "three_one"): 31,
+                ("three", "three_two", "0"): 320,
+                ("three", "three_two", "1"): 321,
+                ("three", "three_two", "2", "four"): 4,
             },
-            "/",
-            ".",
         ),
         (
             {0: {"email": "Not Valid"}, 1: {"name": "What is a name?"}},
-            {"0.email": "Not Valid", "1.name": "What is a name?"},
-            "",
-            ".",
+            {("0", "email"): "Not Valid", ("1", "name"): "What is a name?"},
         ),
     ],
 )
-def test__flatten_dict(inp, outp, prefix, sep):
+def test__flatten_dict(inp, outp):
     """Test flatten dicts results in expected keys."""
-    assert utils.flatten_dict(data=inp, prefix=prefix, separator=sep) == outp
+    assert utils.flatten_dict(data=inp) == outp
